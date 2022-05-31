85a3096c2b581a3be063baed2487621b3f1050c4
Thiaguinzin <nunesthiago08@gmail.com>
2022-05-30 23:43:58 -0300

    feat: entity framework/dbcontext

---
+++ KaemeWebApp/KaemeWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using KaemeWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KaemeWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {   
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
---
+++ KaemeWebApp/KaemeWebApp/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KaemeWebApp.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public string Endereco { get; set; }
+        public string Signo { get; set; }
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+    }
+}
